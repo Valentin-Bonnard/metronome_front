43e74abdd0aa6d05a2f7df0229905e7a3e4deb9b
Valentin-Bonnard <valentinbonnard0303@gmail.com>
2017-07-03 11:22:07 +0200

    change overlay ( use angular directive and jqlite instead classic jquery )

---
+++ app/scripts/directives/header.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name metronomeApp.directive:header
+ * @description
+ * # header
+ */
+angular.module('metronomeApp')
+  .directive('header', function () {
+    return {
+      restrict: 'A',
+      replace: true,
+      templateUrl: '../views/header.html',
+      link: function postLink(scope, element, attrs) {
+        console.log('header load');
+        let openMenu = angular.element(document.querySelector('.open-menu')),
+          closeMenu = angular.element(document.querySelector('.close-menu')),
+          overlay = angular.element(document.querySelector('.overlay'));
+        openMenu.on('click', function () {
+          console.log('Click open menu');
+          overlay.toggleClass('open');
+        });
+
+        closeMenu.on('click', function () {
+          console.log('Click close menu');
+          overlay.removeClass('open');
+        });
+      }
+    };
+  });
